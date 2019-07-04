11299d7476fd2a88a0d258f9bba2a12dfb4618ea
sebastianj1w <sebastianj1w@outlook.com>
2019-07-04 17:18:34 +0800

    wechat-cgi avilable

--- code/wechat-cgi/pages/userinfo/userinfo.js
+++ code/wechat-cgi/pages/userinfo/userinfo.js
@@ -7,6 +7,7 @@ Page({
      * 页面的初始数据
      */
     data: {
+        avatar_src: "../../images/icons/timg.jpg",
         user: {
             username: "sebastianj1w",
             nickname: "Seb",
@@ -14,6 +15,7 @@ Page({
             avatar_id: "1",
             signature: "签名功能不实现"
         },
+        error: false,
         button_message: "编辑个人信息",
         editing: false,
         userInfo: {},
@@ -26,19 +28,30 @@ Page({
         console.log('onLoad')
         var that = this
         //调用应用实例的方法获取全局数据
-        app.getUserInfo(function(userInfo) {
-            //更新数据
-            that.setData({
-                userInfo: userInfo
-            })
-        })
-        wx.request({
-            url: 'https://sebastianj1wzyd.xyz/queryuser?id=1',
-            method: 'GET',
-            success: function (res) {
-                that.setData({user: res.data.data});
-            }
-        })
+        // app.getUserInfo(function(userInfo) {
+        //     //更新数据
+        //     that.setData({
+        //         userInfo: userInfo
+        //     })
+        // })
+        // wx.request({
+        //     url: 'https://sebastianj1wzyd.xyz/queryuser?id=1',
+        //     method: 'GET',
+        //     success: function(res) {
+        //         if (res.data.status === 200) {
+
+        //         } else 
+        //         {
+        //             that.setData({error: true});
+        //         }
+
+        //     }
+        // })
+        this.setData({user: app.globalData.userInfo});
+        that.setData({
+            phone: that.data.user.phone,
+            nickname: that.data.user.nickname
+        });
     },
 
     /**
@@ -91,6 +104,7 @@ Page({
     },
 
     handleClick: function() {
+        let that = this;
         if (!this.data.editing) {
             this.setData({
                 editing: true,
@@ -101,8 +115,45 @@ Page({
                 editing: false,
                 button_message: "编辑个人信息"
             });
+            // console.log(that.data);
+            wx.request({
+                url: 'https://sebastianj1wzyd.xyz/updateuser',
+                method: 'POST',
+                data: {
+                    'id': that.data.user.id,
+                    'phone': that.data.phone,
+                    'nickname': that.data.nickname
+                },
+                success: function(res) {
+                    if (res.statusCode !== 200) {
+                        console.log("error");
+                    } else {
+                        app.globalData.userInfo.phone = that.data.phone;
+                        app.globalData.userInfo.nickname = that.data.nickname;
+
+                    }
+                }
+            })
         }
 
-        console.log(this.data.editing);
-    }
+        // console.log(this.data.editing);
+    },
+
+    handlePhoneInput: function(event) {
+        console.log(1);
+        this.setData({
+            phone: event.detail.detail.value
+        });
+        console.log(this.data);
+    },
+    handleNickInput: function (event) {
+        this.setData({
+            nickname: event.detail.detail.value
+        })
+    },
+    handleSignInput: function (event) {
+        this.setData({
+            signature: event.detail.detail.value
+        })
+    },
 })
