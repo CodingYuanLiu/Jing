fbac48067d9956e35f1816a7589db82b08b0e249
Diving-Fish <dfyshisb@163.com>
2019-07-22 11:53:45 +0800

    add auto switching script

---
+++ code/backend/dev-prod.py
@@ -0,0 +1,120 @@
+def change_file1(path, mode):
+    if mode == '0':
+        fr = open(path, 'r', encoding='utf-8')
+        s = fr.read()
+        fr.close()
+        s = s.replace('\t\tmicro.Address(":8080"),', '\t\t//micro.Address(":8080"),')
+        s = s.replace('k8s.NewService(', 'micro.NewService(')
+        s = s.replace('\tk8s "github.com/micro/kubernetes/go/micro"', '\t//k8s "github.com/micro/kubernetes/go/micro"')
+        fw = open(path, 'w', encoding='utf-8')
+        fw.write(s)
+        fw.close()
+    else:
+        fr = open(path, 'r', encoding='utf-8')
+        s = fr.read()
+        fr.close()
+        s = s.replace('//micro.Address(":8080"),', 'micro.Address(":8080"),')
+        s = s.replace('micro.NewService(', 'k8s.NewService(')
+        s = s.replace('\t//k8s "github.com/micro/kubernetes/go/micro"', '\tk8s "github.com/micro/kubernetes/go/micro"')
+        fw = open(path, 'w', encoding='utf-8')
+        fw.write(s)
+        fw.close()
+
+
+def change_file2(path, mode):
+    if mode == '0':
+        fr = open(path, 'r', encoding='utf-8')
+        s = fr.read()
+        fr.close()
+        s = s.replace('\tos.Setenv("MICRO_REGISTRY", "kubernetes")\n\tclient.DefaultClient = grpc.NewClient(\n\t\tclient.Registry(kubernetes.NewRegistry()),\n\t)',
+                      '\t/*os.Setenv("MICRO_REGISTRY", "kubernetes")\n\tclient.DefaultClient = grpc.NewClient(\n\t\tclient.Registry(kubernetes.NewRegistry()),\n\t)*/')
+        s = s.replace('\t"github.com/micro/go-plugins/client/grpc"', '\t//"github.com/micro/go-plugins/client/grpc"')
+        s = s.replace('\t"github.com/micro/go-plugins/registry/kubernetes"', '\t//"github.com/micro/go-plugins/registry/kubernetes"')
+        s = s.replace('\t"os"', '\t//"os"')
+        fw = open(path, 'w', encoding='utf-8')
+        fw.write(s)
+        fw.close()
+    else:
+        fr = open(path, 'r', encoding='utf-8')
+        s = fr.read()
+        fr.close()
+        s = s.replace(
+            '\t/*os.Setenv("MICRO_REGISTRY", "kubernetes")\n\tclient.DefaultClient = grpc.NewClient(\n\t\tclient.Registry(kubernetes.NewRegistry()),\n\t)*/',
+            '\tos.Setenv("MICRO_REGISTRY", "kubernetes")\n\tclient.DefaultClient = grpc.NewClient(\n\t\tclient.Registry(kubernetes.NewRegistry()),\n\t)')
+        s = s.replace('//"github.com/micro/go-plugins/client/grpc"','"github.com/micro/go-plugins/client/grpc"')
+        s = s.replace('//"github.com/micro/go-plugins/registry/kubernetes"',
+                      '"github.com/micro/go-plugins/registry/kubernetes"')
+        s = s.replace('//"os"', '"os"')
+        fw = open(path, 'w', encoding='utf-8')
+        fw.write(s)
+        fw.close()
+
+
+mode = input("Change mode to development(0) or production(1): ")
+if mode != '0' and mode != '1':
+    print("Mode should be 1 or 0. Aborted.")
+    exit(-1)
+paths = ['./user/user.go', './login/login.go', './activity/main.go']
+paths2 = ['./api-gateway/cli/activity/activity.go', './api-gateway/cli/login/login.go', './api-gateway/cli/user/user.go']
+for p in paths:
+    change_file1(p, mode)
+for p in paths2:
+    change_file2(p, mode)
+if mode == '0':
+    fr = open('./dao/user.go', 'r', encoding='utf-8')
+    s = fr.read()
+    fr.close()
+    s = s.replace('\t//db, err = gorm.Open("mysql", "jing:jing@tcp(localhost:3306)/jing")',
+                  '\tdb, err = gorm.Open("mysql", "jing:jing@tcp(localhost:3306)/jing")')
+    s = s.replace('\tdb, err = gorm.Open("mysql", "jing:jing@tcp(mysql.database:3306)/jing")',
+                  '\t//db, err = gorm.Open("mysql", "jing:jing@tcp(mysql.database:3306)/jing")')
+    fw = open('./dao/user.go', 'w', encoding='utf-8')
+    fw.write(s)
+    fw.close()
+    fr = open('./dao/activity.go', 'r', encoding='utf-8')
+    s = fr.read()
+    fr.close()
+    s = s.replace('\tsession, err := mgo.Dial("mongodb://jing:jing@10.107.149.143:27017/Jing")',
+                  '\t//session, err := mgo.Dial("mongodb://jing:jing@10.107.149.143:27017/Jing")')
+    s = s.replace('\t//session, err := mgo.Dial("mongodb://jing:jing@localhost:27017/Jing")',
+                  '\tsession, err := mgo.Dial("mongodb://jing:jing@localhost:27017/Jing")')
+    fw = open('./dao/activity.go', 'w', encoding='utf-8')
+    fw.write(s)
+    fw.close()
+    fr = open('./api-gateway/main.go', 'r', encoding='utf-8')
+    s = fr.read()
+    fr.close()
+    s = s.replace('\tk8s "github.com/micro/kubernetes/go/web"', '\t//k8s "github.com/micro/kubernetes/go/web"')
+    s = s.replace('k8s.NewService(', 'web.NewService(')
+    fw = open('./api-gateway/main.go', 'w', encoding='utf-8')
+    fw.write(s)
+    fw.close()
+else:
+    fr = open('./dao/user.go', 'r', encoding='utf-8')
+    s = fr.read()
+    fr.close()
+    s = s.replace('\tdb, err = gorm.Open("mysql", "jing:jing@tcp(localhost:3306)/jing")',
+                  '\t//db, err = gorm.Open("mysql", "jing:jing@tcp(localhost:3306)/jing")')
+    s = s.replace('\t//db, err = gorm.Open("mysql", "jing:jing@tcp(mysql.database:3306)/jing")',
+                  '\tdb, err = gorm.Open("mysql", "jing:jing@tcp(mysql.database:3306)/jing")')
+    fw = open('./dao/user.go', 'w', encoding='utf-8')
+    fw.write(s)
+    fw.close()
+    fr = open('./dao/activity.go', 'r', encoding='utf-8')
+    s = fr.read()
+    fr.close()
+    s = s.replace('\t//session, err := mgo.Dial("mongodb://jing:jing@10.107.149.143:27017/Jing")',
+                  '\tsession, err := mgo.Dial("mongodb://jing:jing@10.107.149.143:27017/Jing")')
+    s = s.replace('\tsession, err := mgo.Dial("mongodb://jing:jing@localhost:27017/Jing")',
+                  '\t//session, err := mgo.Dial("mongodb://jing:jing@localhost:27017/Jing")')
+    fw = open('./dao/activity.go', 'w', encoding='utf-8')
+    fw.write(s)
+    fw.close()
+    fr = open('./api-gateway/main.go', 'r', encoding='utf-8')
+    s = fr.read()
+    fr.close()
+    s = s.replace('\t//k8s "github.com/micro/kubernetes/go/web"', '\tk8s "github.com/micro/kubernetes/go/web"')
+    s = s.replace('web.NewService(', 'k8s.NewService(')
+    fw = open('./api-gateway/main.go', 'w', encoding='utf-8')
+    fw.write(s)
+    fw.close()
