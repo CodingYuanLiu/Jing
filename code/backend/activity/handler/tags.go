265f12807215d170ecd4185e86fbdbf505da18dc
sebastianj1w <sebastianj1w@outlook.com>
2019-07-23 09:03:08 +0800

    add logs in tags.go

--- code/backend/activity/handler/tags.go
+++ code/backend/activity/handler/tags.go
@@ -26,6 +26,8 @@ func (actSrv *ActivitySrv) GenTags(ctx context.Context,req *activity.TagReq,resp
 	}
 	resource:= fmt.Sprintf("%s,%s",req.Title,req.Description)
 	words := x.Cut(resource,true)
+	log.Print("words:")
+	log.Println(words)
 	var results []string
 	for _,tag := range tags{
 		for _,word := range words{
@@ -36,6 +38,8 @@ func (actSrv *ActivitySrv) GenTags(ctx context.Context,req *activity.TagReq,resp
 			}
 		}
 	}
+	log.Println("results:")
+	log.Println(results)
 	resp.Tag = results
 	resp.Status = 200
 	return nil
